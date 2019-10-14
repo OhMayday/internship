0cc88859426cc13760bc3bd18608925edac20af8
Gumboy82 <33880742+Gumboy82@users.noreply.github.com>
2019-10-14 11:54:18 -0400

    Add files via upload

---
+++ src/internship/Test.java
@@ -0,0 +1,16 @@
+package internship;
+import java.util.Scanner;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter in the equation for which you want the derivative (Make sure there are spaces between operators!)");
+		String P = scan.nextLine();
+		System.out.println(P);
+		
+		
+	}
+
+}
---
+++ src/internship/lieral.java
@@ -0,0 +1,10 @@
+package internship;
+
+public class lieral {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
