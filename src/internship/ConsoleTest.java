13dfd61a44d12e5b75d3263c25505dd7bb00860a
Gumboy82 <33880742+Gumboy82@users.noreply.github.com>
2019-11-04 17:01:53 -0500

    Add files via upload

--- src/internship/ConsoleTest.java
+++ src/internship/ConsoleTest.java
@@ -1,5 +1,7 @@
 package internship;
-import java.util.Scanner;
+
+
+
 
 public class ConsoleTest {
 
@@ -8,7 +10,7 @@ public class ConsoleTest {
         System.out.print(exp);
     }
 
-    public static void main2(String []args) {
+    public static void main(String []args) {
         System.out.println("Hello World");
         ConsoleTest console = new ConsoleTest();
 
@@ -22,22 +24,4 @@ public class ConsoleTest {
 
         System.out.println("");
     }
-
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter in the equation for which you want the derivative (Make sure there are spaces between operators!)");
-		String P = scan.nextLine();
-		System.out.println(P);
-		
-        String[] tokens = P.split(" ");
-        for(int i = 0; i < tokens.length; ++i)
-        {
-		    System.out.println(tokens[i]);
-            Expression e;
-            if(Literal.isLiteral(tokens[i]))
-                e = new Literal(tokens[i]);
-		        //System.out.println("  is literal");
-        }
-	}
-
 }
